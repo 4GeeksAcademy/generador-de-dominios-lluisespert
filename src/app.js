afff7802abd62e1b64aaa909c47cab38cefde45f
lluisespert <lluisespertc@gmail.com>
2024-11-18 10:46:35 +0000

    Generador de dominios terminado

--- src/app.js
+++ src/app.js
@@ -5,40 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
-let array1 = [
-  "one", 
-  "two", 
-  "three", 
-  "four", 
-  "five"];
+let array1 = ["one", "two", "three", "four", "five"];
 let array2 = [
   "boniato",
   "chupinga",
   "chupacabras",
   "tuercebotas",
-  "abrazafarolas",
+  "abrazafarolas"
 ];
 let array3 = [
   "chupacharcos",
   "cansaliebres",
   "mastuerzo",
   "pescabicho",
-  "fifaproplayer",
+  "fifaproplayer"
 ];
-let array4 = [
-  "luisete", 
-  "bushi", 
-  "loperilla", 
-  "rick", 
-  "moure"];
-let array5 = [
-  "es", 
-  "com", 
-  "cat", 
-  "eu", 
-  "net"];
+let array4 = ["luisete", "bushi", "loperilla", "rick", "moure"];
+let array5 = ["es", "com", "cat", "eu", "net"];
+
+for (let i = 0; i < array1.length; i++) {
+  for (let b = 0; b < array2.length; b++) {
+    for (let c = 0; c < array3.length; c++) {
+      for (let d = 0; d < array4.length; d++) {
+        for (let e = 0; e < array5.length; e++) {
+          console.log(
+            array1[i] + array2[b] + array3[c] + array4[d] + "." + array5[e]
+          );
+        }
+      }
+    }
+  }
+}

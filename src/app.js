a2ae44f5ffd39221bd16adcc51394bfd6c9a86ab
lluisespert <lluisespertc@gmail.com>
2024-11-18 10:00:48 +0000

    Arrays hechos2

--- src/app.js
+++ src/app.js
@@ -9,3 +9,36 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let array1 = [
+  "one", 
+  "two", 
+  "three", 
+  "four", 
+  "five"];
+let array2 = [
+  "boniato",
+  "chupinga",
+  "chupacabras",
+  "tuercebotas",
+  "abrazafarolas",
+];
+let array3 = [
+  "chupacharcos",
+  "cansaliebres",
+  "mastuerzo",
+  "pescabicho",
+  "fifaproplayer",
+];
+let array4 = [
+  "luisete", 
+  "bushi", 
+  "loperilla", 
+  "rick", 
+  "moure"];
+let array5 = [
+  "es", 
+  "com", 
+  "cat", 
+  "eu", 
+  "net"];
